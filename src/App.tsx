1fd8bf6ede0a53cde0d55c53b7cd5d6f46b640ef
KuyaDark <genovarenanjayson@gmail.com>
2025-12-09 23:09:49 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+// App.tsx
+import './App.css';
+import FuzzyText from './component/FuzzyText';
+
+function App() {
+  return (
+    <div className="container">
+      <FuzzyText
+        baseIntensity={0.2}
+        hoverIntensity={0.5}
+        enableHover={true}
+        fontSize={120} // will be overridden responsively in CSS
+        color="#fff"
+      >
+        404
+      </FuzzyText>
+
+      <h2 className="subtitle">Page Not Found</h2>
+
+      <p className="description">
+        Sorry, we are currently performing updates to improve your experience. Please check back later.
+      </p>
+    </div>
+  );
+}
+
+export default App;
